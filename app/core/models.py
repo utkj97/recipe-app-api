046269ba3ec3285c69b1b6b2732a306ff7de1750
utkj97 <utkarzz97@gmail.com>
2019-05-31 23:30:48 +0530

    Added a custom user model

---
+++ app/core/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+
+        if not email:
+            raise ValueError('Users must have an email field.')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+
+        user.set_password(password)
+        # The argument part is needed in case of multiple databases#
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save(self._db)
+
+        return user
+
+
+class UserModel(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of usernames"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    # Sets the field that corresponse to username field#
+    USERNAME_FIELD = 'email'
