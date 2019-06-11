9379a921480b904a1bfd2eb5a20f7f4bed473860
utkj97 <utkarzz97@gmail.com>
2019-06-11 16:33:20 +0530

    Fixed linting issues

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -21,7 +21,7 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
         queryset = self.queryset
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
-  
+
         return queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
